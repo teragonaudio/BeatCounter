cb56ebf91b4094a5c62682380214186a7687cdf3
Nik Reiman <teragonaudio@mac.com>
2010-04-18 01:19:21 +0200

    Migrating to Boolean parameters when necessary

--- source/ParameterFactory.cpp
+++ source/ParameterFactory.cpp
@@ -15,6 +15,10 @@
 #include "MapParameterSet.h"
 #endif
 
+#ifndef __BooleanParameter_h__
+#include "BooleanParameter.h"
+#endif
+
 #ifndef __FloatingPointParameter_h__
 #include "FloatingPointParameter.h"
 #endif
@@ -25,12 +29,12 @@ namespace teragon {
   pluginParameters::PluginParameterSet* ParameterFactory::createPluginParameterSet() {
     MapParameterSet* result = new MapParameterSet();
     
-    result->addParameter(new FloatingPointParameter("Reset", 0.0, 1.0));
+    result->addParameter(new BooleanParameter("Reset"));
     result->addParameter(new FloatingPointParameter("Tolerance", 0.0, 100.0));
     result->addParameter(new FloatingPointParameter("Period", 5.0, 20.0));
-    result->addParameter(new FloatingPointParameter("Autofilter On", 0.0, 1.0));
+    result->addParameter(new BooleanParameter("Autofilter On"));
     result->addParameter(new FloatingPointParameter("Autofilter Freq.", 50.0, 500.0));
-    result->addParameter(new FloatingPointParameter("Link to Host Tempo", 0.0, 1.0));
+    result->addParameter(new BooleanParameter("Link to Host Tempo"));
     
     return result;
   }
